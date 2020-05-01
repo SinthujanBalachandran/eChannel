d92edcf366c53706b5079183ec155ba11c3aab71
SinthujanBalachandran <54698571+SinthujanBalachandran@users.noreply.github.com>
2020-05-01 21:09:23 +0530

    Add files via upload

---
+++ eChanneling/src/main/java/com/example/eChanneling/controllers/LoginController.java
@@ -0,0 +1,30 @@
+package com.example.eChanneling.controllers;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.eChanneling.dtos.PatientLoginDto;
+import com.example.eChanneling.entities.Admin;
+import com.example.eChanneling.services.LoginService;
+
+@RestController
+@RequestMapping("/api")
+public class LoginController {
+	@Autowired
+	private LoginService loginService;
+
+	@PostMapping("/patientLogin")
+	public String patientLogin(@Valid @RequestBody PatientLoginDto patientLoginDto) {
+		return loginService.patientLogin(patientLoginDto);
+		
+	}
+	@PostMapping("/adminLogin")
+	public String adminLogin(@Valid @RequestBody Admin admin) {
+		return loginService.adminLogin(admin);
+	}
+}
